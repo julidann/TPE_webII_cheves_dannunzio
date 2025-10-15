3279b85a1dbdf4572857420a5817350bfd459030
julidann <169107108+julidann@users.noreply.github.com>
2025-10-15 17:45:37 -0300

    Change template file extension to .phtml
    Updated the template file extension from .php to .phtml.

--- app/views/category.view.php
+++ app/views/category.view.php
@@ -4,11 +4,12 @@ class CategoryView {
     public function showCategories ($categories){
         $count = count($categories);
 
-        require_once './templates/categories.table.php';
+        require_once './templates/categories.table.phtml';
     }
 
     public function showError($error) {
         echo "<h1>$error</h1>";
     }
 }
+
 
