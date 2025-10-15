87c552cffdc6c4ee1fae30ec1598ab0b6571e905
julidann <169107108+julidann@users.noreply.github.com>
2025-10-15 17:45:50 -0300

    Change template file extension from .php to .phtml

--- app/views/product.view.php
+++ app/views/product.view.php
@@ -4,11 +4,12 @@ class ProductView {
     public function showProducts ($products){
         $count = count($products);
         
-        require_once './templates/products.table.php';
+        require_once './templates/products.table.phtml';
     }
 
     public function showError($error) {
         echo "<h1>$error</h1>";
     }
 }
+
 
